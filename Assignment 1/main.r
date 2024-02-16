f77f7d301c201135a2c0b4fbc819c0107215021a
FrederikFertin <s203679@student.dtu.dk>
2024-02-16 12:56:06 +0100

    WLS started

--- Assignment 1/main.r
+++ Assignment 1/main.r
@@ -2,20 +2,24 @@
 library("readxl")
 library("ggplot2")
 
-# Import the training and test data
+### DATA IMPORT ###
+# Training Data
 my_data <- read_excel("Assignment 1/DST_BIL54_train.xlsx")
 n_train <- length(my_data)
 train <- unlist(my_data[1, 2:n_train])
 x <- seq(2018, 2022 + 10/12, by = 1 / 12)
+n = length(x)
+y_train = train
 
+# Test Data
 my_data <- read_excel("Assignment 1/DST_BIL54_test.xlsx")
 n_test <- length(my_data)
 test <- unlist(my_data[1, 2:n_test])
-
+y_test = test
 
 
 ##### Q1 #####
-plot(x, train, col = "blue", title("Training data"))
+plot(x, train, col = "blue", title("Training data"), xlab="Year", ylab="Number of Vehicles")
 
 plot(2:n_test, test, col = "red")
 
@@ -41,7 +45,7 @@ sigma2 <- as.numeric(RSS_ols) / (n_train - length(theta))
 
 # Variance and std of estimated errors
 v_ols <- sigma2 * solve(t(x_ols) %*% x_ols)
-s_ols <- sqrt(V_ols)
+s_ols <- sqrt(v_ols)
 
 # Make forecast for next 12 months
 x_forecast <- seq(2022 + 11 / 12, 2023 + 10 / 12, by = 1 / 12)
@@ -72,9 +76,31 @@ plot(x, error)
 # Test error, model overestimates car fleet
 plot(x_forecast[,2], test - y_forecast)
 
+
+
+
 ##### Q3 #####
 
+# 3.1
+lambda = 0.9
+SIGMA_OLS <- diag(n)
+SIGMA_WLS <- diag(n)
+for (i in 1:n) {
+  SIGMA_WLS[i,i] <- 1/lambda^(n-i)
+}
+print(SIGMA_WLS[55:59,55:59])
+W_WLS <- solve(SIGMA_WLS)
 
+# 3.2
+weights <- lambda^((n-1):0)
+# plot the weights:
+barplot(weights, names=1:n)
+
+# 3.3
+sum(weights)
+
+# 3.4
+WLS <- solve(t(x_ols)%*%solve(SIGMA)%*%x_ols)%*%(t(x_ols)%*%solve(SIGMA)%*%y_train)
 
 
 
