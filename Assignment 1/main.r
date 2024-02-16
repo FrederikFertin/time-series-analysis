eb7015f1032105d16bbfeddb67a487833851b72b
FrederikFertin <s203679@student.dtu.dk>
2024-02-16 15:53:10 +0100

    task 3.5 done

--- Assignment 1/main.r
+++ Assignment 1/main.r
@@ -15,7 +15,6 @@ y_train = train
 my_data <- read_excel("Assignment 1/DST_BIL54_test.xlsx")
 n_test <- length(my_data)
 test <- unlist(my_data[1, 2:n_test])
-y_test = test
 
 
 ##### Q1 #####
@@ -48,8 +47,8 @@ v_ols <- sigma2 * solve(t(x_ols) %*% x_ols)
 s_ols <- sqrt(v_ols)
 
 # Make forecast for next 12 months
-x_forecast <- seq(2022 + 11 / 12, 2023 + 10 / 12, by = 1 / 12)
-x_forecast <- cbind(1, x_forecast)
+x_test <- seq(2022 + 11 / 12, 2023 + 10 / 12, by = 1 / 12)
+x_forecast <- cbind(1, x_test)
 y_forecast <- x_forecast %*% theta
 
 # Confidence interval
@@ -70,7 +69,7 @@ ggplot(df_train, aes(x = year, y=train)) +
   geom_point(data = df_forecast, aes(x = year, y = y), col="red", size=.5) +
   geom_ribbon(data=df_forecast, aes(x=year,ymin=CI_lb, ymax=CI_ub), inherit.aes=FALSE, alpha=0.2, fill="blue")
 
-# Risidual of model on historic data, clearly not white noise
+# Residual of model on historic data, clearly not white noise
 plot(x, error)
 
 # Test error, model overestimates car fleet
@@ -97,12 +96,50 @@ weights <- lambda^((n-1):0)
 barplot(weights, names=1:n)
 
 # 3.3
-sum(weights)
+print(sum(weights))
 
 # 3.4
-WLS <- solve(t(x_ols)%*%solve(SIGMA)%*%x_ols)%*%(t(x_ols)%*%solve(SIGMA)%*%y_train)
+WLS <- solve(t(x_ols)%*%solve(SIGMA_WLS)%*%x_ols)%*%(t(x_ols)%*%solve(SIGMA_WLS)%*%y_train)
+print(WLS)
 
+# 3.5
+y_wls <- x_test
+lambdas <- c(0.9, 0.8, 0.7, 0.6)
+df_forecast_wls <- data.frame(year = x_forecast[,2])
+frame_names <- c('l09', 'l08', 'l07', 'l06')
+df_forecast_wls[frame_names] <- NA
 
+df_train[frame_names] <- NA
+
+for (j in 1:length(lambdas)) {
+  lambda = lambdas[j]
+  for (i in 1:n) {
+    SIGMA_WLS[i,i] <- 1/lambda^(n-i)
+  }
+  WLS <- solve(t(x_ols)%*%solve(SIGMA_WLS)%*%x_ols)%*%(t(x_ols)%*%solve(SIGMA_WLS)%*%y_train)
+  y_pred <- x_forecast %*% WLS
+  df_forecast_wls[frame_names[j]] <- y_pred
+  df_train[frame_names[j]] <- x_ols %*% WLS
+}
+
+colors <- c("magenta", "blue", "red", "green")
+
+ggplot(df_train, aes(x = year, y=train, colour='black')) +
+  geom_point(col = "black") +
+  geom_point(data=df_forecast_wls, aes(x=year, y=l09, colour="n9"), size=1)+
+  geom_point(data=df_forecast_wls, aes(x=year, y=l08, colour="n8"), size=1)+
+  geom_point(data=df_forecast_wls, aes(x=year, y=l07, colour="n7"), size=1)+
+  geom_point(data=df_forecast_wls, aes(x=year, y=l06, colour="n6"), size=1)+
+  geom_line(data=df_train, aes(x=year, y=l09, colour="n9"),linewidth=1, alpha=0.5)+
+  geom_line(data=df_train, aes(x=year, y=l08, colour="n8"),linewidth=1, alpha=0.5)+
+  geom_line(data=df_train, aes(x=year, y=l07, colour="n7"),linewidth=1, alpha=0.5)+
+  geom_line(data=df_train, aes(x=year, y=l06, colour="n6"),linewidth=1, alpha=0.5)+
+  theme(legend.position = c(0.8, 0.2), plot.title = element_text(hjust = 0.5)) +
+  scale_color_manual(values = c(n9 = colors[1], n8 = colors[2], n7 = colors[3], n6 = colors[4]),
+                     labels = c(n9 = "\u03bb = 0.9", n8 = "\u03bb = 0.8", n7 = "\u03bb = 0.7", n6 = "\u03bb = 0.6")) +
+  labs(y= "Number of Vehicles", x = "Year", title = "Forecasting 12 months with WLS")
+  
+# 3.6
 
 
 ##### Q4 #####
