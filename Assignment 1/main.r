31e08ee34088e084268cc9dec0f46eff0274e951
JacobSkaarup <jacob.skaarup25@gmail.com>
2024-02-16 10:36:03 +0100

    Fixed data import

--- Assignment 1/main.r
+++ Assignment 1/main.r
@@ -4,12 +4,13 @@ library("readxl")
 # Import the training and test data
 my_data <- read_excel("Assignment 1/DST_BIL54_train.xlsx")
 n_train <- length(my_data)
-train <- my_data[1, 2:n_train]
+train <- unlist(my_data[1, 2:n_train])
 x <- seq(2018, 2022 + 10/12, by = 1 / 12)
 
 my_data <- read_excel("Assignment 1/DST_BIL54_test.xlsx")
 n_test <- length(my_data)
-test <- my_data[1, 2:n_test]
+test <- unlist(my_data[1, 2:n_test])
+
 
 
 ##### Q1 #####
@@ -27,7 +28,8 @@ ones <- rep(1, length(x))
 x_ols <- cbind(ones, x)
 theta <- solve(t(x_ols) %*% x_ols) %*% t(x_ols) %*% train
 
-
+y_hat <- x_ols %*% theta
+plot(x, y_hat, col = "blue", title("Training data"))
 
 
 
