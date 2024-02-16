7e48e2f6d438b78150759e0ccfae700afdc89da0
JacobSkaarup <jacob.skaarup25@gmail.com>
2024-02-16 12:04:11 +0100

    Add ggplot2 library and plot forecast with confidence interval

--- Assignment 1/main.r
+++ Assignment 1/main.r
@@ -1,5 +1,6 @@
 # Required package to read excel
 library("readxl")
+library("ggplot2")
 
 # Import the training and test data
 my_data <- read_excel("Assignment 1/DST_BIL54_train.xlsx")
@@ -44,12 +45,35 @@ s_ols <- sqrt(V_ols)
 
 # Make forecast for next 12 months
 x_forecast <- seq(2022 + 11 / 12, 2023 + 10 / 12, by = 1 / 12)
+x_forecast <- cbind(1, x_forecast)
 y_forecast <- x_forecast %*% theta
 
 # Confidence interval
 alpha <- 0.05
+vmatrix <- sigma2*(1+(x_forecast%*%solve(t(x_ols)%*%x_ols))%*%t(x_forecast))
+
+CI_lb <- y_forecast - qt(alpha / 2, n_train - length(theta)) * sqrt(diag(vmatrix))
+CI_ub <- y_forecast + qt(alpha / 2, n_train - length(theta)) * sqrt(diag(vmatrix))
+
+# Plot fitted model, training data, forecast and CI
+plot(x, train, col = "blue", title("Training data - OLS estimate"),
+     xlim=c(2018, 2024), ylim = c(2.3E6, 2.8E6)) 
+lines(x, y_hat, col = "red")
+points(x_forecast[, 2], y_forecast, col = "green")
+lines(x_forecast[, 2], CI_lb, col = "green")
+lines(x_forecast[, 2], CI_ub, col = "green")
 
 
+df_train <- data.frame(year = x, y = train, y_hat = y_hat)
+df_forecast <- data.frame(year = x_forecast[,2], y = y_forecast, CI_lb = CI_lb, CI_ub = CI_ub)
+
+
+# Ribbon missing.....
+ggplot(df_train, aes(x = year, y=train)) +
+  geom_point() + 
+  geom_line(aes(x = year, y=y_hat), col="red", size = 0.5) +
+  geom_point(data = df_forecast, aes(x = year, y = y), col="red", size=.5) +
+  geom_ribbon(data = df_forecast, aes(x=year, ymin=CI_lb, ymax=CI_ub), inherit.aes=FALSE, alpha=0.8, fill="red")
 
 ##### Q3 #####
 
